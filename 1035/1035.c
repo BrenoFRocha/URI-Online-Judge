674f1c4732ab1801219d6061e87a9b6d9329c337
BrenoFerreiraRocha <brenoferreirarocha@gmail.com>
2017-04-23 23:08:43 -0300

    1035 solution added

---
+++ 1035/1035.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+	int A,B,C,D;
+	
+	scanf("%i",&A);
+	scanf("%i",&B);
+	scanf("%i",&C);
+	scanf("%i",&D);
+	
+	if(B > C && D > A && (D+C) > (A+B) && C >= 0 && D >= 0 && A % 2 == 0)
+	{
+		printf("Valores aceitos\n");
+	}
+	else
+	{
+		printf("Valores nao aceitos\n");
+	}
+	
+	return 0;
+}
